199156a2a9831e65afd751886d0bda04bffcfbbf
Thiago Oliveira <infratl@gmail.com>
2015-06-22 18:14:15 -0700

    https is the new default

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,8 +21,8 @@
 
 # gem_package sources
 if node['rubygems']['gem_disable_default'] then
-  execute "gem sources --remove http://rubygems.org" do
-    only_if "gem sources --list | grep 'http://rubygems.org'"
+  execute "gem sources --remove https://rubygems.org" do
+    only_if "gem sources --list | grep 'https://rubygems.org'"
   end.run_action(:run)
 end
 
@@ -34,8 +34,8 @@ end
 
 # chef_gem sources
 if node['rubygems']['chef_gem_disable_default'] then
-  execute "/opt/chef/embedded/bin/gem sources --remove http://rubygems.org/" do
-    only_if "gem sources --list | grep 'http://rubygems.org'"
+  execute "/opt/chef/embedded/bin/gem sources --remove https://rubygems.org/" do
+    only_if "gem sources --list | grep 'https://rubygems.org'"
   end.run_action(:run)
 end
 
