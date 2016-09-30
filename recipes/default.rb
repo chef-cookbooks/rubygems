6dab3efed5bf5ce1e79e068bf17dc590ead4a5b2
Ryan Hass <rhass@chef.io>
2016-09-29 17:33:00 -0700

    Fixup license and author information.
    [ci skip]

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 # Cookbook Name:: rubygems
 # Recipe:: default
 #
-# Copyright 2009-2016, Chef Inc.
+# Copyright 2009-2016, Chef Software Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
