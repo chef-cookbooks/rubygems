586494339ce1d9919f1450351d9640e47df0569b
Ryan Hass <rhass@chef.io>
2016-10-10 19:46:56 -0700

    Do not attempt to manage home directories.
    This resolves a bug in the `gemrc` provider when using `name :local` or
    `gemrc "local"` in which the resource could potentially recursively
    change file permissions for the home directory. We now skip the
    `directory` resource call within the `gemrc` custom resource when the
    gemrc resource name resolves to either `:local` or `"local"` to ensure
    files and directories do not become tainted when the user or group
    values are set.
    
    Additionally, this cleans up the chefspec slightly and adds a context
    to make it a little cleaner.
    
    Signed-off-by: Ryan Hass <rhass@chef.io>

--- resources/gemrc.rb
+++ resources/gemrc.rb
@@ -46,6 +46,7 @@ action :create do
     user new_resource.user if new_resource.property_is_set?("user")
     group new_resource.group if new_resource.property_is_set?("group")
     recursive true
+    not_if { new_resource.name == :local || new_resource.name == "local" }
   end
 
   file path do
