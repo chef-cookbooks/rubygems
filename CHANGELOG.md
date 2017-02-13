de0b50a483fd7216b683312bdd887f9139192b69
Tim Smith <tsmith@chef.io>
2017-02-13 13:18:26 -0800

    Release 1.0.3
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
 # rubygems Cookbook CHANGELOG
 
 This file is used to list changes made in each version of the rubygems cookbook.
+
+## 1.0.3 (2017-02-13)
+
+- Fix gemrc error if sources is an array.
+- Update ignore files
+- Add maintainers files
+- Add the full Apache license file
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'Ryan Hass <rhass+community-cookbooks@chef.io>'
 license          'Apache-2.0'
 description      'Configures rubygems and bundler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 supports 'amazon'
 supports 'centos'
